dd5dbacfd218153d062fe1ad0318f99640330f58
ghiggi <gionata.ghiggi@gmail.com>
2021-09-02 17:52:19 +0200

    Add sampling, row susetting and robust checks for plotting

--- example.py
+++ example.py
@@ -7,26 +7,77 @@ Created on Wed Sep  1 22:50:08 2021
 """
 #-----------------------------------------------------------------------------.
 import os
-#os.chdir("/home/ghiggi/Projects/pymascdb")
-os.chdir("/home/grazioli/CODES/python/pymascdb")
+os.chdir("/home/ghiggi/Projects/pymascdb")
+#os.chdir("/home/grazioli/CODES/python/pymascdb")
 import mascdb.api
 from mascdb.api import MASC_DB
 
-#dir_path = "/media/ghiggi/New Volume/Data/MASCDB"
-dir_path = "/data/MASC_DB/"
+dir_path = "/media/ghiggi/New Volume/Data/MASCDB"
+#dir_path = "/data/MASC_DB/"
+
+##----------------------------------------------------------------------------.
+### Create MASC_DB instance 
 mascdb = MASC_DB(dir_path=dir_path)
 
-mascdb.plot_flakes(CAM_ID=0, indices=[0], random = True, zoom=True)
+##----------------------------------------------------------------------------.
+## Properties 
+## Property class mascdb
+
+# mascdb.full_db  #  slow !
+mascdb.env   
+mascdb.bs     
+mascdb.gan3d  # Error ... strippa via ... r_g --> r
+
+##----------------------------------------------------------------------------.
+# Property plots 
+mascdb.env.sns.scatterplot(x="T", y="DD", hue="P")
+mascdb.full_db.sns.scatterplot(x="Dmax", y="perim", hue="CAM_ID")
+
+##----------------------------------------------------------------------------.
+# Filtering 
+idx = (mascdb.cam0['Dmax'] > 0.02) & (mascdb.env['RH'] > 50)
+idx = mascdb.cam0['Dmax'] > 0.02
+
+mascdb1 = mascdb.isel(idx)  
+mascdb1.cam0.sns.scatterplot(x="Dmax", y="solidity")
+
+mascdb.isel(idx).cam0.sns.scatterplot(x="Dmax", y="solidity") 
+
+mascdb.isel((mascdb.cam0['Dmax'] > 0.02)).cam0.sns.scatterplot(x="Dmax", y="solidity") 
+
+##----------------------------------------------------------------------------.
+### Selecting specific dataframe variables 
+mascdb.cam0[['Dmax','perim','pix_size']].sns.pairsplot()
+
+##----------------------------------------------------------------------------.
+## Sample 
+mascdb.sample_n(10).cam0
+mascdb.first_n().cam0
+mascdb.last_n().cam0
+mascdb.tail().cam0
+mascdb.head().cam0
+
+##----------------------------------------------------------------------------.
+## Image plots 
+
+mascdb.plot_flake(CAM_ID=0, random = True, zoom=True)
+mascdb.plot_flake(CAM_ID=0, random = False, zoom=True)
 mascdb.plot_flake(CAM_ID=0, index=0, random = True, zoom=True)
+mascdb.plot_flake(CAM_ID=[0], index=[0], random = True, zoom=True)
 
+mascdb.plot_flakes(CAM_ID=0, indices=[0], random = True, zoom=True)
+mascdb.plot_flakes(CAM_ID=0, indices=0, random = True, zoom=True) # ERROR
+mascdb.plot_flakes(CAM_ID=0, indices=None, random = True, n_images = 1, zoom=True)
 mascdb.plot_flakes(CAM_ID=0, indices=None, random = True, n_images = 2, zoom=True)
-mascdb.plot_flakes(CAM_ID=0, indices=None, random = True, n_images = 9, zoom=True)
+mascdb.plot_flakes(CAM_ID=[0], indices=None, random = True, n_images = 9, zoom=True) 
 mascdb.plot_flakes(CAM_ID=0, indices=[1,2], random = True, n_images = 2, zoom=True)
-mascdb.plot_flakes(CAM_ID=0, indices=[1,2], random = True, n_images = 9, zoom=True) # this should raise warning ?
+mascdb.plot_flakes(CAM_ID=0, indices=[1,2], random = True, n_images = 9, zoom=True)  
 
+mascdb.plot_triplets(indices=[0,10], random = True, n_triplets = 1, zoom=True)
 mascdb.plot_triplets(indices=None, random = True, n_triplets = 1, zoom=True)
 mascdb.plot_triplets(indices=None, random = True, n_triplets = 3, zoom=True)
 
+##----------------------------------------------------------------------------.
 ## Added accessor for seaborn plot routines ;) 
 # - hue: categorical group over which to compute stats 
 # - https://seaborn.pydata.org/examples/index.html: Gallery from which to draw examples :) 
--- mascdb/api.py
+++ mascdb/api.py
@@ -10,6 +10,24 @@ import xarray as xr
 import pandas as pd
 import numpy as np
 import mascdb.pd_sns_accessor 
+import copy
+# TODO 
+# - RANDOM TO FALSE 
+
+# TODO: add stuff of mm @Jacobo did 
+# add option to report mm ... 
+# pix_size = self.triplet.loc[index].pix_size*1e3  # mm
+# xsize = out.shape[0]*pix_size # mm
+# ysize = out.shape[1]*pix_size # mm
+
+#-----------------------------------------------------------------------------.
+### In future ###
+## filter
+# mascdb = mascdb.filter("cam0.Dmax > 2 and cam1.Xhi > 2")
+# mascdb.filter("cam0.Dmax > 2 and cam1.Xhi > 2")
+# mascdb.filter(triplet.Dmax > 2 and cam1.Xhi > 2)
+# mascdb.sel(label)
+#-----------------------------------------------------------------------------.
 
 def _internal_bbox(img):
     rows = np.any(img, axis=1)
@@ -31,6 +49,83 @@ def _center_image(img, nrow, ncol):
     arr[slice(row_incr,row_incr+r), slice(col_incr,col_incr+c)] = img 
     return arr 
 
+#-----------------------------------------------------------------------------.
+### Checks 
+
+def _check_CAM_ID(CAM_ID): 
+    "Return CAM_ID integer."
+    if not isinstance(CAM_ID, (int, np.int64, list)):
+        raise TypeError("'CAM_ID', if specified, must be an integer or list (of length 1).")
+    if isinstance(CAM_ID, list):
+        if len(CAM_ID) != 1:
+           raise ValueError("Expecting a single value for 'CAM_ID'.")
+        CAM_ID = int(CAM_ID[0])
+    # Check value validity
+    if CAM_ID not in [0,1,2]:
+        raise ValueError("Valid values of 'CAM_ID' are [0,1,2].")
+    # Return integer CAM_ID
+    return CAM_ID
+
+def _check_index(index, vmax):
+    "Return index integer."
+    if not isinstance(index, (int, np.int64, list)):
+        raise TypeError("'index', if specified, must be an integer or list (of length 1).")
+    if isinstance(index, list):
+        if len(index) != 1:
+           raise ValueError("Expecting a single value for 'index'.")
+        index = int(index[0])
+    # Check value validity
+    if index < 0:
+        raise ValueError("'index' must be a positive integer.")
+    if index > vmax:
+        raise ValueError("The largest 'index' can be {}".format(vmax))
+    # Return integer index
+    return index
+
+def _check_indices(indices, vmax):
+    "Return index integer."
+    if not isinstance(indices, (int, np.int64, list)):
+        raise TypeError("'indices', if specified, must be an integer or list (of length 1).")
+    if isinstance(indices, (int, np.int64)):
+        indices = [indices]
+    # Check value validity
+    if any([not isinstance(index, (int, np.int64)) for index in indices]):
+        raise ValueError("All 'indices' values must be integers.")
+    if any([index < 0 for index in indices]):
+        raise ValueError("All 'indices' values must be positive integers.")
+    if any([index > vmax for index in indices]):
+        raise ValueError("The largest 'indices' value can be {}".format(vmax))
+    # Return list of indices
+    return indices
+
+def _check_n_triplets(n_triplets, vmax):
+    if not isinstance(n_triplets, int):
+        raise TypeError("'n_triplets' must be an integer.")
+    if n_triplets < 1:
+        raise ValueError("'n_triplets' must be at least 1.")    
+    if n_triplets > vmax: 
+        raise ValueError("'n_triplets' must be maximum {}.".format(vmax))
+    if n_triplets > 10: 
+        raise ValueError("It's not recommended to plot more than 10 triplets of images.")       
+
+def _check_n_images(n_images, vmax):
+    if not isinstance(n_images, int):
+        raise TypeError("'n_images' must be an integer.")
+    if n_images < 1:
+        raise ValueError("'n_images' must be at least 1.")    
+    if n_images > vmax: 
+        raise ValueError("'n_images' must be maximum {}.".format(vmax))
+    if n_images > 25: 
+        raise ValueError("It's not recommended to plot more than 25 images.")
+        
+def _check_random(random): 
+    if not isinstance(random, bool):
+        raise TypeError("'random' must be either True or False.")
+        
+def _check_zoom(zoom): 
+    if not isinstance(zoom, bool):
+        raise TypeError("'zoom' must be either True or False.")
+        
 #-----------------------------------------------------------------------------.
 class MASC_DB:
     """
@@ -56,22 +151,144 @@ class MASC_DB:
         self.cam1    = pd.read_parquet(cam1_fpath)
         self.cam2    = pd.read_parquet(cam2_fpath)
         self.triplet = pd.read_parquet(triplet_fpath)
+        
+        # Number of triplets 
+        self.n_triplets = len(self.triplet)
+        
+    ##------------------------------------------------------------------------.
+    #################
+    ## Subsetting ###
+    #################
+    def isel(self, idx): 
+        # Copy new instance 
+        self = copy.deepcopy(self)
+        #---------------------------------------------------------------------.
+        # TODO: Check valid idx 
+        if isinstance(idx, pd.Series):
+            if idx.dtype.name == 'bool':
+                idx = np.where(idx)[0]
+            else: 
+                idx = np.array(idx)
+        # if list of bool --> np.where 
+        idx = list(idx)
 
+        ##--------------------------------------------------------------------.
+        # Subset all datasets 
+        self.da = self.da.isel(TripletID=idx)
+        if isinstance(idx[0], bool):
+            self.cam0 = self.cam0[idx]  
+            self.cam1 = self.cam1[idx]  
+            self.cam2 = self.cam2[idx]  
+            self.triplet = self.triplet[idx]  
+        else: 
+            self.cam0 = self.cam0.iloc[idx]
+            self.cam1 = self.cam1.iloc[idx]
+            self.cam2 = self.cam2.iloc[idx]
+            self.triplet = self.triplet.iloc[idx]
+        ##--------------------------------------------------------------------.
+        # Update number of triplets 
+        self.n_triplets = len(self.triplet)
+        return self 
+    
+    def sample_n(self, n=10):
+        idx = np.random.choice(self.n_triplets, n) 
+        return self.isel(idx)
+    
+    def first_n(self, n=10):     
+        idx = np.arange(n)
+        return self.isel(idx)
+    
+    def head(self, n=10):     
+        idx = np.arange(n)
+        return self.isel(idx)
+    
+    def tail(self, n=10):     
+        idx = np.arange(self.n_triplets-1,self.n_triplets-n-1, step=-1)
+        return self.isel(idx)
+    
+    def last_n(self, n=10):     
+        idx = np.arange(self.n_triplets-1,self.n_triplets-n-1, step=-1)
+        return self.isel(idx)
+    
+   
+    ##------------------------------------------------------------------------.
+    ################
+    ### Getters ####
+    ################
+    @property
+    def env(self):
+        columns = list(self.triplet.columns)
+        env_variables = [column for column in columns if column.startswith("env_")]
+        env_db = self.triplet[[*env_variables]]
+        env_db.columns = [column.strip("env_") for column in env_variables]
+        return env_db
+    
+    @property
+    def bs(self):
+        columns = list(self.triplet.columns)
+        bs_variables = [column for column in columns if column.startswith("bs_")]
+        bs_db = self.triplet[[*bs_variables]]
+        bs_db.columns = [column.strip("bs_") for column in bs_variables]
+        return bs_db
+    
+    @property
+    def gan3d(self):
+        # TODO: to modify 3dgan_ to gan3d_ in future
+        columns = list(self.triplet.columns)
+        gan3d_variables = [column for column in columns if column.startswith("3dgan_")]
+        gan3d_db = self.triplet[[*gan3d_variables]]
+        gan3d_db.columns = [column.strip("3dgan_") for column in gan3d_variables]
+        return gan3d_db
+    
+    @property
+    def full_db(self):
+        triplet = self.triplet.copy()
+        # Add CAM_ID to each cam db 
+        l_cams = [self.cam0.copy(), self.cam1.copy(), self.cam2.copy()]
+        for i, cam in enumerate(l_cams):
+             cam['CAM_ID'] = i
+        # Merge cam(s) db into 
+        full_db = pd.concat(l_cams)
+        # Add triplet variables to fulldb 
+        # TODO: to remove/modify in future
+        labels_vars = ['riming_deg_level', 'riming_id',  
+                       'melting_id',
+                       'melting_prob',  
+                       'label_name',       
+                       'label_id',
+                       'label_id_prob']
+        vars_not_add = ['pix_size','Xhi','n_roi', 'Dmax'] + labels_vars
+        triplet = triplet.drop(columns=vars_not_add)
+        full_db = full_db.merge(triplet, how="left")
+        return full_db
+    
+    ##------------------------------------------------------------------------.
+    ###########################
+    ### Plotting routines #####
+    ###########################
 
     def plot_triplets(self, indices=None, random = True, n_triplets = 1, zoom=True, **kwargs):
+        #--------------------------------------------------.
+        # Retrieve number of valid index
+        n_idxs = len(self.triplet.index)
+        if n_idxs == 0: 
+            raise ValueError("No data to plot.") 
+        #--------------------------------------------------.
         # Check args
-        if indices is not None: 
-            # Check valid indices 
-            # TODO: 
-            random = False
-        if random: # (use df.sample)
-            indices = list(pd.Index(np.random.choice(self.triplet.index, n_triplets)))
-        # TODO: check max n_triplets and len(indices) !!!
-        # TODO: add stuff of mm @Jacobo did 
-        # add option to report mm ... 
-        # pix_size = self.triplet.loc[index].pix_size*1e3  # mm
-        # xsize = out.shape[0]*pix_size # mm
-        # ysize = out.shape[1]*pix_size # mm
+        _check_random(random)
+        _check_zoom(zoom)
+        _check_n_triplets(n_triplets, vmax=n_idxs)
+     
+        #--------------------------------------------------.
+        # Define index if is not provided
+        if indices is None:
+            if random: # (use df.sample)
+               indices = list(np.random.choice(n_idxs, n_triplets))
+            else: 
+               indices = list(np.arange(0,n_triplets))
+        #--------------------------------------------------.       
+        # Check validity of indices
+        indices = _check_indices(indices, vmax=n_idxs-1)
         #--------------------------------------------------.
         # Subset triplet(s) images 
         da_subset = self.da.isel(TripletID = indices).transpose(...,'CAM_ID','TripletID')
@@ -89,7 +306,7 @@ class MASC_DB:
             # Reassign to da_subset 
             da_subset = da_subset.isel(x=slice(0, c_max), y=slice(0,r_max))
             l_stack = [np.stack(l_zoomed[i*3:3*(i+1)], axis=-1) for i in range(len(indices))]
-            new_arr = np.stack(l_stack, axis=-1) # TODO: check order is correct 
+            new_arr = np.stack(l_stack, axis=-1)  
             da_subset.values = new_arr
         #--------------------------------------------------.
         # Plot triplet(s)
@@ -112,26 +329,32 @@ class MASC_DB:
             
     def plot_flake(self, CAM_ID=None, index=None, random = True, zoom=True, **kwargs):
         # Check args
-        if index is not None: 
-            if not isinstance(index, int):
-                raise TypeError("'plot_flake' expects 'index' as an integer.")
-        if random: # (use df.sample)
-           if index is None:
-               index = list(pd.Index(np.random.choice(self.triplet.index, 1)))
-           if CAM_ID is None: 
-                CAM_ID = np.random.choice([0,1,2], 1)
-             
-        # TODO: check max n_triplets and len(indices) !!!
-        # TODO: check valid CAM_ID if not None (int value, list... )
-
-        # TODO: add stuff of mm @Jacobo did 
-        # add option to report mm ... 
-        # pix_size = self.triplet.loc[index].pix_size*1e3  # mm
-        # xsize = out.shape[0]*pix_size # mm
-        # ysize = out.shape[1]*pix_size # mm
+        _check_random(random)
+        _check_zoom(zoom)
+        #--------------------------------------------------.
+        # Retrieve number of valid index
+        n_idxs = len(self.triplet.index)
+        if n_idxs == 0: 
+            raise ValueError("No data to plot.")
+        #--------------------------------------------------.
+        # Define index if is not provided
+        if index is None:
+           if random:         
+               index = list(np.random.choice(n_idxs, 1))[0]
+           else:             
+               index = 0   
+        if CAM_ID is None: 
+            if random:    
+               CAM_ID = np.random.choice([0,1,2], 1)
+            else:
+               CAM_ID = 1
+        #--------------------------------------------------.       
+        # Check validty of CAM_ID and index 
+        CAM_ID = _check_CAM_ID(CAM_ID)
+        index = _check_index(index, vmax=n_idxs-1)
         #--------------------------------------------------.
         # Subset triplet(s) images 
-        # - If CAM_ID is an integer (instead of list length 1 ... the CAM_ID dimension is dropped)
+        # - If CAM_ID is an integer (instead of list of length 1), then the CAM_ID dimension is dropped)
         da_img = self.da.isel(TripletID = index, CAM_ID = CAM_ID) 
         #--------------------------------------------------.
         # Zoom all images to same extent 
@@ -143,43 +366,48 @@ class MASC_DB:
             da_img = da_img.isel(x=slice(0, c_max), y=slice(0,r_max))
             da_img.values = zoomed_img
         #--------------------------------------------------.
-        # Plot triplet(s)
-        p = da_img.plot(x='x',y='y', # TODO no aspect without size 
+        # Plot single image 
+        # - TODO: 'aspect' cannot be specified without 'size'
+        p = da_img.plot(x='x',y='y', 
                         cmap='gray', add_colorbar=False, 
                         vmin=0, vmax=255,
                         **kwargs)
         #--------------------------------------------------. 
         return p  
 
-    def plot_flakes(self, CAM_ID=None, indices=None, random = True, n_images = 9, zoom=True, **kwargs):
-        # Check args
-        if indices is not None: 
-            if isinstance(indices, int):
-                indices = list(indices)
-            # Check valid indices 
-            # TODO: 
-            random = False
-        if random: # (use df.sample)
-           if indices is None:
-               indices = list(pd.Index(np.random.choice(self.triplet.index, n_images)))
-           if CAM_ID is None: 
-                CAM_ID = np.random.choice([0,1,2], 1)
-        print(len(indices))
-        if len(indices) == 1: 
-            print("You should use 'plot_flake' to plot a single image.")
-            return self.plot_flake(index=indices[0], CAM_ID=CAM_ID, random=False, zoom=zoom, *kwargs)
-            
-        # TODO: check max n_images and len(indices) !!!
-        # TODO: check valid CAM_ID if not None (int value, list... )
-        
+    def plot_flakes(self, CAM_ID=None, indices=None, random = True, 
+                    n_images = 9, zoom=True, 
+                    col_wrap = 3, **kwargs):
+        # Retrieve number of valid index
+        n_idxs = len(self.triplet.index) # TODO 
         # TODO: 
         # - Option to stack to ImageID ... and then sample that ... title will report TripletID and CAM ID
-        
-        # TODO: add stuff of mm @Jacobo did 
-        # add option to report mm ... 
-        # pix_size = self.triplet.loc[index].pix_size*1e3  # mm
-        # xsize = out.shape[0]*pix_size # mm
-        # ysize = out.shape[1]*pix_size # mm
+
+        #--------------------------------------------------.
+        # Check args
+        _check_random(random)
+        _check_zoom(zoom)
+        _check_n_images(n_images, vmax=n_idxs)
+        #--------------------------------------------------
+        # Define index if is not provided
+        if indices is None:
+            if random:  
+               indices = list(np.random.choice(n_idxs, n_images))
+            else: 
+               indices = list(np.arange(0,n_images))
+        if CAM_ID is None: 
+            if random:    
+                CAM_ID = np.random.choice([0,1,2], 1)[0]
+            else:
+                CAM_ID = 1
+        #-------------------------------------------------- 
+        # Check validity of indices and CAM_ID
+        indices = _check_indices(indices, vmax=n_idxs-1)
+        CAM_ID = _check_CAM_ID(CAM_ID)
+        #-------------------------------------------------- 
+        if len(indices) == 1: 
+            print("It's recommended to use 'plot_flake()' to plot a single image.")
+            return self.plot_flake(index=indices[0], CAM_ID=CAM_ID, random=random, zoom=zoom, *kwargs)
         #--------------------------------------------------.
         # Subset triplet(s) images 
         # - If CAM_ID is an integer (instead of list length 1 ... the CAM_ID dimension is dropped)
@@ -203,7 +431,7 @@ class MASC_DB:
         # Plot triplet(s)
         row = "TripletID" if len(indices) > 1 else None
         p = da_subset.plot(x='x',y='y', 
-                           row="TripletID", col_wrap=3, 
+                           row=row, col_wrap=col_wrap, 
                            aspect=1, 
                            cmap='gray', add_colorbar=False, 
                            vmin=0, vmax=255,
@@ -217,6 +445,6 @@ class MASC_DB:
         #--------------------------------------------------. 
         return p  
      
-
+    ##------------------------------------------------------------------------.
 
  
