ec221798ad3d5454c84eaee01c654fa1ca0aeee7
Jacopo Grazioli <jacopo.grazioli@epfl.ch>
2021-11-25 10:25:42 +0100

    Test

--- tutorial/00_download_mascdb.py
+++ tutorial/00_download_mascdb.py
@@ -5,4 +5,21 @@ Created on Wed Sep 15 11:27:30 2021
 
 @author: ghiggi
 """
-# TODO wrapper function that download data from zenodo directly 
+
+
+# Permanent Zenodo Link to most recent database version
+zenodo_link = "https://doi.org/10.5281/zenodo.5578920"
+
+
+"""
+There are only 4 files to download. Pragmatically this is best done by hand.
+
+However, if you are interested we may recommend a tool named "zenodo_get"
+https://zenodo.org/record/1261813#.YZd8LLso9H4
+
+
+"""
+
+
+
+
